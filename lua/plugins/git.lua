584e42ce36834572728f3369da64eab05e4d7c00
SavvyHex <paisaketh@gmail.com>
2024-12-07 09:53:20 +0530

    Initial Commit

---
+++ lua/plugins/git.lua
@@ -0,0 +1,30 @@
+return {
+	{
+		-- Git signs
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+			signs_staged = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+	{
+		-- Git Integration
+		"tpope/vim-fugitive",
+	},
+	{
+		-- GitHub Integration
+		"tpope/vim-rhubarb",
+	},
+}
