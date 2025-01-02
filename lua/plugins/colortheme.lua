0f288588171482bc70ab4fead22e90d13fe634e7
SavvyHex <paisaketh@gmail.com>
2025-01-02 08:39:25 +0530

    vantablack bufferline workaround

--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,16 @@
+-- Color scheme for the editor
 return {
-	"kdheepak/monochrome.nvim",
-	name = "monochrome",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("monochrome")
-	end,
+	{
+		"kdheepak/monochrome.nvim",
+		name = "monochrome",
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("monochrome")
+		end,
+	},
+	{
+		"slugbyte/lackluster.nvim",
+		lazy = false,
+		priority = 1000,
+	},
 }
