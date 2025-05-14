78cae065701dd0ba2648fa26fe01da3b191e5b7d
Saketh Sunil Pai <paisaketh@gmail.com>
2025-05-14 12:42:02 +0530

    comment keybind

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,10 +4,10 @@ return {
 	opts = {},
 	config = function()
 		local opts = { noremap = true, silent = true }
-		vim.keymap.set("n", "<C-/>", require("Comment.api").toggle.linewise.current, opts)
+		vim.keymap.set("n", "<C-_>", require("Comment.api").toggle.linewise.current, opts)
 		vim.keymap.set(
 			"v",
-			"<C-/>",
+			"<C-_>",
 			"<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>",
 			opts
 		)
